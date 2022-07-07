ef40380bcc894ad56bfc4cdd5cda475e6f02769d
Gaurav Das <gaurav2016das@gmail.com>
2022-07-08 00:08:29 +0530

    Time: 24 ms (12.78%), Space: 54.4 MB (94.24%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,16 +1,29 @@
-public class Solution {
-    public int maximalSquare(char[][] matrix) {
-        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
-        int[][] dp = new int[rows + 1][cols + 1];
-        int maxsqlen = 0;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i-1][j-1] == '1'){
-                    dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
+class Solution {
+    private int dp[][];
+    private int maxArea(char[][] matrix,int row,int col)
+    {
+
+        int max=0;
+        for(int i=1;i<=row;i++)
+        {
+            for(int j=1;j<=col;j++)
+            {
+                if(matrix[i-1][j-1]=='1')
+                {
+                    dp[i][j]=Math.min(Math.min(dp[i][j-1],dp[i-1][j-1]),dp[i-1][j])+1;
+                    max=Math.max(dp[i][j],max);
                 }
             }
         }
-        return maxsqlen * maxsqlen;
+        return max;
+       
+    }
+    public int maximalSquare(char[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[m-1].length;
+        dp=new int[m+1][n+1];
+        int side=maxArea(matrix,m,n);
+        System.out.println(Arrays.deepToString(dp));
+        return side*side;
     }
 }
