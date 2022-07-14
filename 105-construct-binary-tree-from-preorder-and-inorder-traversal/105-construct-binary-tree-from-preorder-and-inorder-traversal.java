738d893523003330ceae9bcc112f653f857aad86
Gaurav Das <gaurav2016das@gmail.com>
2022-07-14 23:46:25 +0530

    Time: 7 ms (20.23%), Space: 44.7 MB (28.97%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -13,6 +13,38 @@
  *     }
  * }
  */
+/*
+ Preorder traversal -> root,left subtree,right subtree
+ Inorder traversal -> left subtree,root,right subtree
+ 
+ Let's consider the basic of binary tree, There's a left subtree and a right subtree and a root.Both the subtree is also a binary tree and follow the same approach again.
+ 
+ So, preorder traversal will provide the root of each subtree.
+ Now, we know the very first element of preorder will be our root.
+ 
+ And moving sequentially in preorder will provide the root of each left subtree and once left subtree is explored compeletely, Then it will provide root of right subtree.
+ 
+ So for each root from Preorder, we can decide the left subtree and right subtree from inorder array.
+ 
+ In simple words,
+ 
+ inorder: [a,b,c] 
+ preorder: [b,a,c]
+ 
+ from preorder we know,b is root.
+ from inorder, we know a is left subtree and c is right subtree.
+ b is determined.
+ now,
+ both subtree can be further divided into
+ 
+ left subtree: { inorder:[a] and preorder:[a] }
+ right subtree: { inorder: [c] and preorder: [c] }
+ 
+ This is valid for all subtree and provide a recurrence relation.
+ 
+ NOTE: Not a DP problem.There's no overlapping of sub-problems.Backtracking is the right approach
+ 
+*/
 class Solution {
     private int inorder[],preorder[];
     private int pStart=0;
