4c509b99facecfc7fb6ecdade4e2ca66df3402fc
Gaurav Das <gaurav2016das@gmail.com>
2022-07-23 21:52:01 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (38.46%) - LeetHub

---
+++ 2347-best-poker-hand/2347-best-poker-hand.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String bestHand(int[] ranks, char[] suits) {
+        String hands="High Card";
+        boolean isFlush=true;
+        int i;
+        char card=suits[0];
+        for( i=0;i<ranks.length;i++)
+        {
+            if(suits[i]!=card)
+                isFlush=false;
+            int count=1;
+            for(int j=i+1;j<ranks.length;j++)
+            {
+                if(ranks[i]==ranks[j])
+                    count++;
+            }
+            if(count>=3)
+                hands="Three of a Kind";
+            else if(count==2&&!hands.equals("Three of a Kind"))
+                hands="Pair";
+        }
+        if(isFlush)
+            hands="Flush";
+        
+        return hands;   
+    }
+}
