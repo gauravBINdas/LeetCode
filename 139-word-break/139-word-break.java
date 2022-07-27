725e10e0e62edc462172dea7ff4f9416ebdedf69
Gaurav Das <gaurav2016das@gmail.com>
2022-07-28 01:29:57 +0530

    Time: 41 ms (5.41%), Space: 54.2 MB (5.51%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,35 @@
+class Solution {
+    private char chars[];
+    private List<String> wordDict;
+    private Map<String,Boolean> map=new HashMap<>();
+    private boolean word(String s,int curr)
+    {
+        String key=s+Integer.toString(curr);
+        if(!map.containsKey(key)){
+            boolean res;
+            if(curr>=chars.length)
+            {
+                res=wordDict.contains(s);
+            }
+            else
+            {
+                String next=Character.toString(chars[curr]);
+                if(wordDict.contains(s))
+                {
+                    res=word(next,curr+1)||word(s+next,curr+1);
+                }
+                else
+                {
+                    res=word(s+next,curr+1);
+                }
+            }
+                map.put(key,res);
+        }
+        return map.get(key);
+    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+       chars=s.toCharArray();
+       this.wordDict=wordDict;
+       return word("",0);
+    }
+}
