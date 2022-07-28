489c4bba4ba6ab887a0a45e24d7f34fee2622ee5
Gaurav Das <gaurav2016das@gmail.com>
2022-07-28 22:20:21 +0530

    Time: 4 ms (85.70%), Space: 44 MB (49.16%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,31 +1,45 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s.length();i++)
+        // Map<Character,Integer> map=new HashMap<>();
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        // }
+        // for(int i=0;i<t.length();i++)
+        // {
+        //     if(map.containsKey(t.charAt(i))){
+        //         int freq=map.get(t.charAt(i))-1;
+        //         if(freq>0){
+        //             map.put(t.charAt(i),freq);
+        //         }
+        //         else
+        //         {
+        //             map.remove(t.charAt(i));
+        //         }
+        //     }
+        //     else
+        //     {
+        //         return false;
+        //     }
+        // }
+        // if(map.isEmpty())
+        //     return true;
+        // else
+        //     return false;
+        int chars[]=new int[26];
+        for(char c: s.toCharArray())
         {
-            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+            chars[c-'a']++;
         }
-        for(int i=0;i<t.length();i++)
+        for(char c: t.toCharArray())
         {
-            if(map.containsKey(t.charAt(i))){
-                int freq=map.get(t.charAt(i))-1;
-                if(freq>0){
-                    map.put(t.charAt(i),freq);
-                }
-                else
-                {
-                    map.remove(t.charAt(i));
-                }
-            }
-            else
-            {
+            chars[c-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(chars[i]!=0)
                 return false;
-            }
         }
-        if(map.isEmpty())
-            return true;
-        else
-            return false;
-        
+        return true;
     }
 }
