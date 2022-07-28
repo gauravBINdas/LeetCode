1c489e33611ef8924a312684cd544a293e184f5e
Gaurav Das <gaurav2016das@gmail.com>
2022-07-28 21:56:06 +0530

    Time: 8 ms (10.48%), Space: 70.9 MB (83.32%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -3,6 +3,10 @@ class Solution {
         int max=0;
         int left=0;
         int right=height.length-1;
+        //Initially left=0 and right=length is giving the max width possible
+        // After that width will always decrease by 1 unit whether I increment left or decrement right.So ,the only factor remains to determine max area is height.
+        //Now at curr iteration , I know my max height.I have to figure out if there's any max height possible in other end in next iteration.
+        // continue till left surpasses right.
         while(left<right)
         {
             int width=right-left;
