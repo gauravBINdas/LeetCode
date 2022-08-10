508c2d0e8cde98fec273f6395be38b8bcc744aa4
Gaurav Das <gaurav2016das@gmail.com>
2022-08-10 23:28:15 +0530

    Time: 6 ms (40.58%), Space: 57.3 MB (59.07%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.java
@@ -0,0 +1,46 @@
+class Solution {
+    private char grid[][];
+    private boolean visited[][];
+    private boolean isRange(int row,int col)
+    {
+        if(row<0||row>=grid.length)
+            return false;
+        if(col<0||col>=grid[row].length)
+            return false;
+        return true;
+    }
+    private int dfs(int row,int col)
+    {
+        if(isRange(row,col)&&!visited[row][col]){
+            if(grid[row][col]=='1')
+            {
+                visited[row][col]=true;
+                dfs(row,col+1);
+                dfs(row+1,col);
+                dfs(row,col-1);
+                dfs(row-1,col);
+                
+            }
+            return 1;
+        }
+        else
+            return 0;
+    }
+    public int numIslands(char[][] grid) {
+        int sum=0;
+        this.grid=grid;
+        visited=new boolean[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[i].length;j++)
+            {
+                   if(grid[i][j]=='1')
+                   {
+                        sum=sum+dfs(i,j);
+                      //  System.out.println("i:"+i+"j:"+j+"sum:"+sum);
+                   }
+            }
+        }
+        return sum;
+    }
+}
