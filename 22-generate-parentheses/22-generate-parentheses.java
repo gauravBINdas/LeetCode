b23577f8d247529fc46c5611168642ee313f23ce
Gaurav Das <gaurav2016das@gmail.com>
2022-08-23 21:43:17 +0530

    Time: 5 ms (21.89%), Space: 45.3 MB (10.36%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,30 +1,30 @@
 class Solution {
-    List<String> bucket=new ArrayList<>();
-    private void makeParenthesis(int l,int r,String formed)
+    private List<String> makeParenthesis(int l,int r,String formed)
     {
         String newFormed;
+        List<String> bucket=new ArrayList<>();
         if(l!=0||r!=0){
-              if(l>0)
-              {
-                  newFormed=formed+"(";
-                  makeParenthesis(l-1,r,newFormed);
-              }
-              if(l!=r && r>0)
-              {
-                  newFormed=formed+")";
-                  makeParenthesis(l,r-1,newFormed);
-              }
-            }
+          if(l>0)
+          {
+              newFormed=formed+"(";
+              bucket.addAll(makeParenthesis(l-1,r,newFormed));
+          }
+          if(l!=r && r>0)
+          {
+              newFormed=formed+")";
+              bucket.addAll(makeParenthesis(l,r-1,newFormed));
+          }
+        }
         else
         {
             bucket.add(formed);
         }
+        return bucket;
     }
     public List<String> generateParenthesis(int n) {
         String newFormed="(";
         int l=n-1;
         int r=n;
-        makeParenthesis(l,r,newFormed);
-        return bucket;
+        return makeParenthesis(l,r,newFormed);
     }
 }
